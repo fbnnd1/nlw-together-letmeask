588cf7f4e1f5414414e46e176e2528529a0fdca9
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2021-06-27 12:58:23 -0300

    feat: Page 404

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Home} from './pages/Home';
 import {NewRoom} from './pages/NewRoom';
 import {Room} from './pages/Room';
 import { AdminRoom } from './pages/AdminRoom';
+import { Page404 } from './pages/Page404';
 
 import {AuthContextProvider} from './contexts/AuthContext';
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
           <Route path="/myroom/:id" component={AdminRoom} />
+          <Route path="/*" component={Page404} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ src/pages/Page404.tsx
@@ -0,0 +1,12 @@
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/page404.scss';
+
+export function Page404 () {
+    return (
+        <div className="main-content">
+            <img src={logoImg} alt="Letmeask" />
+            <h1>Página não localizada</h1>
+        </div>
+    );
+}
